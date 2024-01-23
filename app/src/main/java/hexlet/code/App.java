3cb75a7f06c0404704ad896503f11e326af8b885
Aleksandra Volkova <91-ksandra@mail.ru>
2024-01-23 17:55:47 +0600

    Stage 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,7 +7,18 @@ public class App {
     @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
             description = "Compares two configuration files and shows a difference.")
 
+//
+
     static class GenDiff implements Runnable {
+
+        @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+        private String filepath1;
+        @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+        private String filepath2;
+
+        @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+        private String format;
+
         @Override
         public void run() {
             //   Differ.generate();
