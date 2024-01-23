9a5382f253a8dea509982ddad88758e52344e958
Aleksandra Volkova <91-ksandra@mail.ru>
2024-01-23 16:57:41 +0600

    Stage 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,21 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true,
+            description = "Compares two configuration files and shows a difference.")
+
+    static class GenDiff implements Runnable {
+        @Override
+        public void run() {
+            //   Differ.generate();
+        }
     }
 
+    public static void main(String[] args) {
+        new CommandLine(new GenDiff()).execute(args);
+        System.out.println("Hello World!");
+    }
 }
